d8458a157982c1839dad2631b343ba5bd2adf9ad
israel mahalla <israel.mahalla@gmail.com>
2023-05-01 22:41:47 +0300

    rename functions and orginized code

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import smtplib
 
-global user, recipient, subject, text
-success = None
-counter = 3
+session = smtplib.SMTP("smtp.gmail.com", 587)
+session.starttls()
 
 
 def establish_connection(user, passwd):
@@ -16,36 +15,35 @@ def establish_connection(user, passwd):
         return is_success
 
 
-def gets_user_and_passwd(counter):
+def get_user_and_passwd():
     user = input("Enter your email address : ")
     passwd = input("Enter your password : ")
-    print("trying to authenticate... ")
-    success = establish_connection(user, passwd)
-    if not success:
-        counter -= 1
-        print(f"You have {counter} attempts left")
-    return counter, success, user
+    return user, passwd
 
 
-def gets_rec_subject_text():
+def get_recipient_address_subject_and_text():
     recipient = input("Enter recipient's email address : ")
     subject = input("Enter email subject : ")
     text = input("Enter your message : ")
     return recipient, subject, text
 
 
-session = smtplib.SMTP("smtp.gmail.com", 587)
-session.starttls()
+def send_mail():
+    success = None
+    counter = 3
+    while not success and counter != 0:
+        user, passwd = get_user_and_passwd()
+        print("trying to authenticate... ")
+        success = establish_connection(user, passwd)
+        if not success:
+            counter -= 1
+            print(f"You have {counter} attempts left")
+        else:
+            recipient, subject, text = get_recipient_address_subject_and_text()
+            message = f'Subject: {subject}\n\n{text}'
+            session.sendmail(user, recipient, message)
+            session.quit()
+            print('Message sent :)')
 
-while not success and counter != 0:
-    counter, success, user = gets_user_and_passwd(counter)
-    if success:
-        recipient, subject, text = gets_rec_subject_text()
-        message = f'Subject: {subject}\n\n{text}'
-        session.sendmail(user, recipient, message)
-        session.quit()
-        print('Message sent :)')
-    else:
-        continue
 
-
+send_mail()
